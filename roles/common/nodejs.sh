1e2cb238f65fb9769a9e1706a4e4f1fa19b87ba0
jeevan <jeev5324u@gmail.con>
2024-03-14 15:03:04 +0530

    ll

--- roles/common/nodejs.sh
+++ roles/common/nodejs.sh
@@ -6,42 +6,18 @@
     name: nodejs
     state: installed
 
-- name: add user
-  ansible.builtin.user:
-  name: roboshop
-
-- name: removing old app content
-  ansible.builtin.file:
-    path: /app
-    state: absent
-
-- name: creating app directory
-  ansible.builtin.file:
-    path: /app
-    state: directory
-
-- name: downloading {{component}} app content
-  ansible.builtin.unarchive:
-    src: curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/{{component}}.zip
-    dest: /app
+- name: app pre-req
+  ansible.builtin.include_tasks: "pre-req.yml"
 
 - name: installing nodejs dependencies
   community.general.npm:
     path: /app
     state: latest
 
-- name: setting {{component}} service file
-  ansible.builtin.copy:
-    src: "{{component}}.serrvice"
-    dest: /etc/systemd/system/{{component}}.service
+- name: settingup systemd
+  ansible.builtin.include_tasks: "systemd.yml"
 
-- name: installing {{component}} service
-  ansible.builtin.systemd:
-    name: "{{component}}"
-    daemon_reload: yes
-    enabled: yes
-    OB
-    state: restarted
+    
 - name: load schema
   ansible.builtin.include_tasks: "{{schema_type}}"-schema.yml
   when: shema_load
